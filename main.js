4c782a6c3f1b412f084b53eacb59cc6d26e3474a
JNBhuucdj8 <86115113+JNBhuucdj8@users.noreply.github.com>
2021-06-18 17:00:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//const year = 2021;
+//let useryear = prompt("Введите ваш год рождения") 
+//altrt(`Ваш возраст ${2021-YearOfBirth}`);
+//alert(`Ваш возраст ${year-userYear} `);
+const name = Oleg;
+let username = prompt("Введите ваше Имя");
+//altrt(Ваше имя {Oleg-nameOfBirth});
+alert("Ваше имя {name-username}" );
